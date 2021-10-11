627b16cd1335d408ae63ab73ff0a9432a3bebfec
Sungyong Cho <to.sungyongcho@gmail.com>
2021-10-11 21:35:23 +0900

    fix: TypeORM keepConnectionAlive 설정 추가

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -20,7 +20,7 @@ const config: ConnectionOptions = {
   // Allow both start:prod and start:dev to use migrations
   // __dirname is either dist or src folder, meaning either
   // the compiled js in prod or the ts in dev.
-  migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+  migrations: [`${__dirname}/migrations/*{.ts,.js}`],
   cli: {
     entitiesDir: 'src/entities',
     migrationsDir: 'src/migrations',
