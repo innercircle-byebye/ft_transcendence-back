a36f3a4d65644948b5fbb7682ba41f4bdfc530fb
khcho902 <khcho902@gmail.com>
2021-10-18 03:33:53 +0900

    docs: block api들에 swagger 달아둠

--- src/relation/relation.controller.ts
+++ src/relation/relation.controller.ts
@@ -8,6 +8,13 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserDto } from 'src/user/dto/user.dto';
 import { RelationService } from './relation.service';
 
 @UseGuards(AuthGuard('jwt'))
@@ -15,6 +22,16 @@ import { RelationService } from './relation.service';
 export class RelationController {
   constructor(private relationService: RelationService) {}
 
+  @ApiTags('Block')
+  @ApiOperation({
+    summary: '차단목록 조회',
+    description: '내가 차단한 사용자들의 목록을 조회한다.',
+  })
+  @ApiOkResponse({
+    type: UserDto,
+    isArray: true,
+    description: '내가 차단한 사용자들의 목록',
+  })
   @Get('/block/list')
   async getBlockedUserList(@Req() req) {
     const { user } = req;
@@ -22,16 +39,19 @@ export class RelationController {
     const blockedUserList = await this.relationService.getBlockedUserList(
       user.userId,
     );
-
-    return {
-      success: true,
-      code: 200,
-      data: {
-        blockedUserList,
-      },
-    };
+    return blockedUserList;
   }
 
+  @ApiTags('Block')
+  @ApiOperation({
+    summary: '차단하기',
+    description: '특정 사용자를 나한테 차단한다.',
+  })
+  @ApiResponse({
+    status: 201,
+    type: UserDto,
+    description: '차단된 사용자 정보',
+  })
   @Post('block/:block_user_id')
   async blockUser(@Req() req, @Param('block_user_id') blockUserId: number) {
     const { user } = req;
@@ -40,16 +60,15 @@ export class RelationController {
       user.userId,
       blockUserId,
     );
-
-    return {
-      success: true,
-      code: 201,
-      data: {
-        blockedUser,
-      },
-    };
+    return blockedUser;
   }
 
+  @ApiTags('Block')
+  @ApiOperation({
+    summary: '차단 해제하기',
+    description: '내가 차단한 사용자를 차단 해제한다.',
+  })
+  @ApiOkResponse({ type: UserDto, description: '차단 해제된 사용자 정보' })
   @Delete('block/:unblock_user_id')
   async unblockUser(
     @Req() req,
@@ -61,13 +80,6 @@ export class RelationController {
       user.userId,
       unblockUserId,
     );
-
-    return {
-      success: true,
-      code: 200,
-      data: {
-        unblockedUser,
-      },
-    };
+    return unblockedUser;
   }
 }
