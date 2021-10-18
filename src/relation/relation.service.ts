fb637555c5ec57df7a8f3b2dd3001791a39c28d3
Sungyong Cho <to.sungyongcho@gmail.com>
2021-10-18 17:15:05 +0900

    chore: relation.service.ts unblockUser 주석 삭제

--- src/relation/relation.service.ts
+++ src/relation/relation.service.ts
@@ -77,14 +77,7 @@ export class RelationService {
       throw new BadRequestException('차단된 기록이 없습니다.');
     }
 
-    // TODO: softRemove 쓰는걸로 바꿔보자.
-    // previousBlockData.isDeleted = true;  // 이게 적용이 안됨.
-    // await this.blockRepository.softRemove(previousBlockData);
-    previousBlockData.deletedAt = new Date();
-    await this.blockRepository.update(
-      previousBlockData.blockId,
-      previousBlockData,
-    );
+    await this.blockRepository.softRemove(previousBlockData);
 
     const { currentHashedRefreshToken, ...unblockedUser } = unblockUser;
     return unblockedUser;
