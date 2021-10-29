ad059733e58cf223cff1b82e70f949135ae4e1c4
Sungyong Cho <to.sungyongcho@gmail.com>
2021-10-30 01:00:00 +0900

    chore: 채널 유저 업데이트  ban 로직 수정

--- src/channel/channel.service.ts
+++ src/channel/channel.service.ts
@@ -351,10 +351,9 @@ export class ChannelService {
     if (targetUser.banDate && banDate !== null)
       throw new BadRequestException('ban 당한 사용자입니다.');
 
-    if (typeof Object(mutedDate) !== undefined)
-      targetUser.mutedDate = mutedDate;
+    if (Object(mutedDate) !== undefined) targetUser.mutedDate = mutedDate;
 
-    if (typeof Object(banDate) !== undefined) {
+    if (Object(banDate) !== undefined) {
       targetUser.banDate = banDate;
       if (banDate === null)
         return this.channelMemberRepository.recover(targetUser);
