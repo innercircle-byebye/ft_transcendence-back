c5c822232496b7021cf7476d1be2872a8c24a2e4
Sungyong Cho <to.sungyongcho@gmail.com>
2021-11-03 06:55:05 +0900

    feat: 게임 module, service, controller 추가

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -4,12 +4,14 @@ import { GameObserver } from 'src/entities/GameObserver';
 import { GameResult } from 'src/entities/GameResult';
 import { GameRoom } from 'src/entities/GameRoom';
 import { User } from 'src/entities/User';
+import { GameService } from './game.service';
+import { GameController } from './game.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, GameRoom, GameResult, GameObserver]),
   ],
-  // providers: [ChannelService],
-  // controllers: [ChannelController],
+  providers: [GameService],
+  controllers: [GameController],
 })
 export class GameModule {}
