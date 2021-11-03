43075bd7c0e6c51b2e77fb79f8fa798268333aed
Sungyong Cho <to.sungyongcho@gmail.com>
2021-11-04 04:18:15 +0900

    chore: 더미 데이터 seeding 추가

---
+++ src/database/game-init-data.ts
@@ -0,0 +1,154 @@
+import { GameMemberStatus } from 'src/entities/GameMember';
+import { BallSpeed } from 'src/entities/GameResult';
+
+const GameRoomInitData = [
+  {
+    gameRoomId: 1,
+    title: '게임방 1--player1만 있음 (관전 수용인원 0)',
+    password: null,
+    maxParticipantNum: 2,
+  },
+  {
+    gameRoomId: 2,
+    title: '게임방 2--player 1, 2만 있음 (관전 수용인원 0)',
+    password: null,
+    maxParticipantNum: 2,
+  },
+  {
+    gameRoomId: 3,
+    title: '게임방 3--player 1, 2만 있음 (관전 수용인원 2)',
+    password: null,
+    maxParticipantNum: 4,
+  },
+  {
+    gameRoomId: 4,
+    title: '게임방 4 -- 수용인원 꽉참 (player1, player2, 관전 수용인원 2)',
+    password: null,
+    maxParticipantNum: 4,
+  },
+  {
+    gameRoomId: 5,
+    title: '게임방 5--비밀번호 테스트',
+    password: '$2a$12$tzssFSe5ixvQ/2/nggDsRefJHpJk/QkQLwS8o5NFM9jJ676CmCjmy',
+    maxParticipantNum: 10,
+  },
+];
+
+const GameMemberInitData = [
+  {
+    gameRoomId: 1,
+    userId: 1,
+    status: GameMemberStatus.PLAYER_ONE,
+  },
+  {
+    gameRoomId: 2,
+    userId: 2,
+    status: GameMemberStatus.PLAYER_ONE,
+  },
+  {
+    gameRoomId: 2,
+    userId: 3,
+    status: GameMemberStatus.PLAYER_TWO,
+  },
+  {
+    gameRoomId: 3,
+    userId: 4,
+    status: GameMemberStatus.PLAYER_ONE,
+  },
+  {
+    gameRoomId: 3,
+    userId: 5,
+    status: GameMemberStatus.PLAYER_TWO,
+  },
+  {
+    gameRoomId: 4,
+    userId: 6,
+    status: GameMemberStatus.PLAYER_ONE,
+  },
+  {
+    gameRoomId: 4,
+    userId: 7,
+    status: GameMemberStatus.PLAYER_TWO,
+  },
+  {
+    gameRoomId: 4,
+    userId: 8,
+    status: GameMemberStatus.OBSERVER,
+  },
+  {
+    gameRoomId: 4,
+    userId: 9,
+    status: GameMemberStatus.OBSERVER,
+  },
+];
+
+const GameResultInitData = [
+  {
+    gameRoomId: 1,
+    playerOneId: 1,
+    playerTwoId: null,
+    playerOneScore: null,
+    playerTwoScore: null,
+    winPoint: 10,
+    ballSpeed: BallSpeed.MEDIUM,
+    startAt: null,
+    endAt: null,
+  },
+  {
+    gameRoomId: 2,
+    playerOneId: 2,
+    playerTwoId: 3,
+    playerOneScore: 10,
+    playerTwoScore: 8,
+    winPoint: 10,
+    ballSpeed: BallSpeed.MEDIUM,
+    startAt: new Date('2021-11-04 00:40:00.000'),
+    endAt: new Date('2021-11-04 00:45:00.000'),
+  },
+  {
+    gameRoomId: 2,
+    playerOneId: 2,
+    playerTwoId: 3,
+    playerOneScore: 15,
+    playerTwoScore: 10,
+    winPoint: 15,
+    ballSpeed: BallSpeed.MEDIUM,
+    startAt: new Date('2021-11-04 00:50:00.000'),
+    endAt: new Date('2021-11-04 00:55:00.000'),
+  },
+  {
+    gameRoomId: 2,
+    playerOneId: 2,
+    playerTwoId: 3,
+    playerOneScore: 15,
+    playerTwoScore: 10,
+    winPoint: 15,
+    ballSpeed: BallSpeed.MEDIUM,
+    startAt: new Date('2021-11-04 01:00:00.000'),
+    endAt: null,
+  },
+  {
+    gameRoomId: 3,
+    playerOneId: 4,
+    playerTwoId: 5,
+    playerOneScore: 7,
+    playerTwoScore: 7,
+    winPoint: 10,
+    ballSpeed: BallSpeed.FAST,
+    startAt: new Date('2021-11-04 01:00:00.000'),
+    endAt: null,
+  },
+  {
+    gameRoomId: 4,
+    playerOneId: 6,
+    playerTwoId: 7,
+    playerOneScore: null,
+    playerTwoScore: null,
+    winPoint: 8,
+    ballSpeed: BallSpeed.SLOW,
+    startAt: null,
+    endAt: null,
+  },
+];
+
+export { GameRoomInitData, GameMemberInitData, GameResultInitData };
