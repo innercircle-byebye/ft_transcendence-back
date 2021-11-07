b175c4ee6b0a6e46250f129cd7725849c73adc1e
khcho902 <khcho902@gmail.com>
2021-11-07 12:34:57 +0900

    feat: 게임에서 사용할 기본 세팅값 추가

---
+++ src/events/game/SETTINGS.ts
@@ -0,0 +1,19 @@
+export const SETTINGS = {
+  WIDTH: 600,
+  HEIGHT: 400,
+  PLAYER: {
+    WIDTH: 20,
+    HEIGHT: 80,
+    GAP: 30,
+  },
+  BALL: {
+    WIDTH: 15,
+    HEIGHT: 15,
+  },
+  GOAL: 5,
+};
+
+export const CLIENT_SETTINGS = {
+  WIDTH: SETTINGS.WIDTH,
+  HEIGHT: SETTINGS.HEIGHT,
+};
