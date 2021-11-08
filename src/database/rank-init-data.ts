72de4c433e2c3927ea8fee2818885dcc98b65878
Sungyong Cho <to.sungyongcho@gmail.com>
2021-11-08 16:42:10 +0900

    feat: Rank 더미데이터 생성

---
+++ src/database/rank-init-data.ts
@@ -0,0 +1,22 @@
+const rankInitData = [
+  {
+    rankId: 1,
+    title: '브론즈',
+    criteriaExperience: 421,
+    imagePath: 'image/rank/bronze.jpg',
+  },
+  {
+    rankId: 2,
+    title: '실버',
+    criteriaExperience: 841,
+    imagePath: 'image/rank/silver.jpg',
+  },
+  {
+    rankId: 3,
+    title: '골드',
+    criteriaExperience: 1261,
+    imagePath: 'image/rank/gold.jpg',
+  },
+];
+
+export { rankInitData };
