cf477ecc1c4fe8dbb969bdfc4c5b807324fe1fa5
Sungyong Cho <to.sungyongcho@gmail.com>
2021-11-08 22:38:52 +0900

    fix: DmService 추가

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -5,6 +5,7 @@ import { GameRoom } from 'src/entities/GameRoom';
 import { User } from 'src/entities/User';
 import { GameMember } from 'src/entities/GameMember';
 import { DmModule } from 'src/dm/dm.module';
+import { DmService } from 'src/dm/dm.service';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
 
@@ -13,7 +14,7 @@ import { GameController } from './game.controller';
     TypeOrmModule.forFeature([User, GameRoom, GameResult, GameMember]),
     DmModule,
   ],
-  providers: [GameService],
+  providers: [GameService, DmService],
   controllers: [GameController],
 })
 export class GameModule {}
